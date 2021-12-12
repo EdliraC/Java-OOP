6d76b00b91aaf820a95087842b2d96abed9fc0e8
EdliraC <94520903+EdliraC@users.noreply.github.com>
2021-12-12 20:44:20 +0100

    Te reja

---
+++ Chapter8/src/TrafficLight.java
@@ -0,0 +1,24 @@
+//Write an enum type TrafficLight, whose constants (RED, GREEN, YELLOW) take one parameter—
+//the duration of the light.Write a program to test the TrafficLight enum so that it displays the
+//enum constants and their durations.
+
+public enum TrafficLight {
+	
+	RED(20),
+	GREEN(35),
+	YELLOW(15);
+	
+	private final int kohezgjatjaSek;
+	
+	TrafficLight(int sek)
+	{
+		this.kohezgjatjaSek=sek;
+	}
+	
+	
+	public int gjejKohezgjatje()
+	{
+		return this.kohezgjatjaSek;
+	}
+
+}
---
+++ Chapter8/src/TrafficLightTest.java
@@ -0,0 +1,15 @@
+
+public class TrafficLightTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(TrafficLight ngjyre: TrafficLight.values())
+		{
+			System.out.printf(" %s -%d%n", ngjyre, ngjyre.gjejKohezgjatje());
+		}
+		
+
+	}
+
+}
